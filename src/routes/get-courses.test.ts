9083b94fedf59a83901d27807edab14039301cba
Matheus Moreira <matheustjp111@gmail.com>
2025-08-14 15:11:09 -0300

    feat: get courses test

---
+++ src/routes/get-courses.test.ts
@@ -0,0 +1,31 @@
+import { randomUUID } from 'node:crypto'
+
+import request from 'supertest'
+import { expect, test } from 'vitest'
+
+import { server } from '../app.ts'
+import { makeCourse } from '../tests/factories/make-course.ts'
+
+test('get courses', async () => {
+  await server.ready()
+
+  const titleId = randomUUID()
+
+  await makeCourse(titleId)
+
+  const response = await request(server.server).get(
+    `/courses?search=${titleId}`,
+  )
+
+  expect(response.status).toEqual(200)
+  expect(response.body).toEqual({
+    total: 1,
+    courses: [
+      {
+        id: expect.any(String),
+        title: titleId,
+        enrollments: 0,
+      },
+    ],
+  })
+})
