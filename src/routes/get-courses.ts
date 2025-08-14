8049dbac2e6c358107eccfcb71187b66fe50c756
Matheus Moreira <matheustjp111@gmail.com>
2025-08-14 14:43:21 -0300

    feat: ensure search and pagination on get courses

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,8 +1,9 @@
+import { and, asc, count, eq, ilike, type SQL } from 'drizzle-orm'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 
 import { db } from '../db/client.ts'
-import { courses } from '../db/schema.ts'
+import { courses, enrollments } from '../db/schema.ts'
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -11,27 +12,52 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ['courses'],
         summary: 'Get all courses',
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(['id', 'title']).optional().default('id'),
+          page: z.coerce.number().optional().default(1),
+        }),
         response: {
           200: z.object({
             courses: z.array(
               z.object({
                 id: z.uuid(),
                 title: z.string(),
+                enrollments: z.number(),
               }),
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db
-        .select({
-          id: courses.id,
-          title: courses.title,
-        })
-        .from(courses)
+      const { search, orderBy, page } = request.query
 
-      return reply.send({ courses: result })
+      const conditions: SQL[] = []
+
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`))
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({
+            id: courses.id,
+            title: courses.title,
+            enrollments: count(enrollments.id),
+          })
+          .from(courses)
+          .leftJoin(enrollments, eq(enrollments.courseId, courses.id))
+          .orderBy(asc(courses[orderBy]))
+          .offset((page - 1) * 10)
+          .limit(10)
+          .where(and(...conditions))
+          .groupBy(courses.id),
+        db.$count(courses, and(...conditions)),
+      ])
+
+      return reply.send({ courses: result, total })
     },
   )
 }
